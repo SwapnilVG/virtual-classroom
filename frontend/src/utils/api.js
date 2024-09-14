a4bd6ab7510523ef74ae5b869d858cfced6f482d
SwapnilVG <swapnilgadekar2000@gmail.com>
2024-09-14 19:16:41 +0530

    update code

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { getToken } from './auth.js';
 
 const api = axios.create({
-  baseURL: "http://localhost:3000"
+  baseURL: "http://localhost:3000/api/"
 });
 
 api.interceptors.request.use((config) => {
